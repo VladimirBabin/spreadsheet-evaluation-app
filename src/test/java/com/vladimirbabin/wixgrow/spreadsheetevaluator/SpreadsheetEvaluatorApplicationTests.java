8807d76aca7119ae85f895dfaaf4f9864a860375
VladimirBabin <vladimir.sol.rojo@gmail.com>
2023-04-23 20:31:10 +0300

    Added FormulaInfoTest.

---
+++ src/test/java/com/vladimirbabin/wixgrow/spreadsheetevaluator/SpreadsheetEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirbabin.wixgrow.spreadsheetevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadsheetEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
