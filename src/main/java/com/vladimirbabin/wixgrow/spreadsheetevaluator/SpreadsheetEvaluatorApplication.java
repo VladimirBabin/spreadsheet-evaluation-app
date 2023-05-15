89db16e04a6611f842f53d8cecbdf7053bf4c498
vicria <vicria@inbox.ru>
2023-05-15 12:49:34 -0300

    так собирается

--- src/main/java/com/vladimirbabin/wixgrow/spreadsheetevaluator/SpreadsheetEvaluatorApplication.java
+++ src/main/java/com/vladimirbabin/wixgrow/spreadsheetevaluator/SpreadsheetEvaluatorApplication.java
@@ -1,7 +1,5 @@
 package com.vladimirbabin.wixgrow.spreadsheetevaluator;
 
-import com.vladimirbabin.wixgrow.spreadsheetevaluator.dto.Spreadsheet;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
